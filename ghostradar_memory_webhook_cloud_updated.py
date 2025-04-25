ee72553738d7b72728addb3ad73b4b4dca4c4640
fevko <158847464+fevko@users.noreply.github.com>
2025-04-25 17:21:54 +0400

    Update ghostradar_memory_webhook_cloud_updated.py

--- ghostradar_memory_webhook_cloud_updated.py
+++ ghostradar_memory_webhook_cloud_updated.py
@@ -45,4 +45,5 @@ def webhook():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
